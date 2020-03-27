bed8499eadf55718a07f9ad7674291aaef04e974
Jihad Benastey <jbenastey@gmail.com>
2020-03-27 11:59:35 +0700

    simpan matriks kriteria ke db

--- app/Http/Controllers/AhpController.php
+++ app/Http/Controllers/AhpController.php
@@ -16,7 +16,7 @@ class AhpController extends Controller
     {
         //
         $data['kuesioner'] = DB::table('kuesioner')->get();
-        return view('ahp.index',$data);
+        return view('ahp.index', $data);
     }
 
     /**
@@ -32,7 +32,7 @@ class AhpController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -43,16 +43,16 @@ class AhpController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
         //
         $data['kuesioner'] = DB::table('kuesioner')
-            ->where('kuesioner_id',$id)
+            ->where('kuesioner_id', $id)
             ->first();
-        return view('ahp.lihat',$data);
+        return view('ahp.lihat', $data);
     }
 
     public function matriksKriteria($id)
@@ -60,9 +60,9 @@ class AhpController extends Controller
         //
 //        var_dump($id);die;
         $kuesioner = DB::table('kuesioner')
-            ->where('kuesioner_id',$id)
+            ->where('kuesioner_id', $id)
             ->first();
-        $skala = json_decode($kuesioner->kuesioner_ks,true);
+        $skala = json_decode($kuesioner->kuesioner_ks, true);
         $matriks = [
             'HR' => [
                 'HR' => 1,
@@ -100,96 +100,106 @@ class AhpController extends Controller
                 'QK' => 1,
             ],
         ];
-//        for ($i = 1; $i <= count($skala) ; $i++) {
-//            var_dump($skala['ks'.$i]);
-            if ($skala['ks1'] > 0){
-                $matriks['HR']['CS'] = abs($skala['ks1']);
-                $matriks['CS']['HR'] = abs(1/$skala['ks1']);
-            } else {
-                $matriks['HR']['CS'] = abs(1/$skala['ks1']);
-                $matriks['CS']['HR'] = abs($skala['ks1']);
-            }
+        if ($skala['ks1'] > 0) {
+            $matriks['HR']['CS'] = abs($skala['ks1']);
+            $matriks['CS']['HR'] = abs(1 / $skala['ks1']);
+        } else {
+            $matriks['HR']['CS'] = abs(1 / $skala['ks1']);
+            $matriks['CS']['HR'] = abs($skala['ks1']);
+        }
 
-            if ($skala['ks2'] > 0){
-                $matriks['HR']['EH'] = abs($skala['ks2']);
-                $matriks['EH']['HR'] = abs(1/$skala['ks2']);
-            } else {
-                $matriks['HR']['EH'] = abs(1/$skala['ks2']);
-                $matriks['EH']['HR'] = abs($skala['ks2']);
-            }
+        if ($skala['ks2'] > 0) {
+            $matriks['HR']['EH'] = abs($skala['ks2']);
+            $matriks['EH']['HR'] = abs(1 / $skala['ks2']);
+        } else {
+            $matriks['HR']['EH'] = abs(1 / $skala['ks2']);
+            $matriks['EH']['HR'] = abs($skala['ks2']);
+        }
 
-            if ($skala['ks3'] > 0){
-                $matriks['HR']['SR'] = abs($skala['ks3']);
-                $matriks['SR']['HR'] = abs(1/$skala['ks3']);
-            } else {
-                $matriks['HR']['SR'] = abs(1/$skala['ks3']);
-                $matriks['SR']['HR'] = abs($skala['ks3']);
-            }
+        if ($skala['ks3'] > 0) {
+            $matriks['HR']['SR'] = abs($skala['ks3']);
+            $matriks['SR']['HR'] = abs(1 / $skala['ks3']);
+        } else {
+            $matriks['HR']['SR'] = abs(1 / $skala['ks3']);
+            $matriks['SR']['HR'] = abs($skala['ks3']);
+        }
 
-            if ($skala['ks4'] > 0){
-                $matriks['HR']['QK'] = abs($skala['ks4']);
-                $matriks['QK']['HR'] = abs(1/$skala['ks4']);
-            } else {
-                $matriks['HR']['QK'] = abs(1/$skala['ks4']);
-                $matriks['QK']['HR'] = abs($skala['ks4']);
-            }
+        if ($skala['ks4'] > 0) {
+            $matriks['HR']['QK'] = abs($skala['ks4']);
+            $matriks['QK']['HR'] = abs(1 / $skala['ks4']);
+        } else {
+            $matriks['HR']['QK'] = abs(1 / $skala['ks4']);
+            $matriks['QK']['HR'] = abs($skala['ks4']);
+        }
 
-            if ($skala['ks5'] > 0){
-                $matriks['CS']['EH'] = abs($skala['ks5']);
-                $matriks['EH']['CS'] = abs(1/$skala['ks5']);
-            } else {
-                $matriks['CS']['EH'] = abs(1/$skala['ks5']);
-                $matriks['EH']['CS'] = abs($skala['ks5']);
-            }
+        if ($skala['ks5'] > 0) {
+            $matriks['CS']['EH'] = abs($skala['ks5']);
+            $matriks['EH']['CS'] = abs(1 / $skala['ks5']);
+        } else {
+            $matriks['CS']['EH'] = abs(1 / $skala['ks5']);
+            $matriks['EH']['CS'] = abs($skala['ks5']);
+        }
 
-            if ($skala['ks6'] > 0){
-                $matriks['CS']['SR'] = abs($skala['ks6']);
-                $matriks['SR']['CS'] = abs(1/$skala['ks6']);
-            } else {
-                $matriks['CS']['SR'] = abs(1/$skala['ks6']);
-                $matriks['SR']['CS'] = abs($skala['ks6']);
-            }
+        if ($skala['ks6'] > 0) {
+            $matriks['CS']['SR'] = abs($skala['ks6']);
+            $matriks['SR']['CS'] = abs(1 / $skala['ks6']);
+        } else {
+            $matriks['CS']['SR'] = abs(1 / $skala['ks6']);
+            $matriks['SR']['CS'] = abs($skala['ks6']);
+        }
 
-            if ($skala['ks7'] > 0){
-                $matriks['CS']['QK'] = abs($skala['ks7']);
-                $matriks['QK']['CS'] = abs(1/$skala['ks7']);
-            } else {
-                $matriks['CS']['QK'] = abs(1/$skala['ks7']);
-                $matriks['QK']['CS'] = abs($skala['ks7']);
-            }
+        if ($skala['ks7'] > 0) {
+            $matriks['CS']['QK'] = abs($skala['ks7']);
+            $matriks['QK']['CS'] = abs(1 / $skala['ks7']);
+        } else {
+            $matriks['CS']['QK'] = abs(1 / $skala['ks7']);
+            $matriks['QK']['CS'] = abs($skala['ks7']);
+        }
 
-            if ($skala['ks8'] > 0){
-                $matriks['EH']['SR'] = abs($skala['ks8']);
-                $matriks['SR']['EH'] = abs(1/$skala['ks8']);
-            } else {
-                $matriks['EH']['SR'] = abs(1/$skala['ks8']);
-                $matriks['SR']['EH'] = abs($skala['ks8']);
-            }
+        if ($skala['ks8'] > 0) {
+            $matriks['EH']['SR'] = abs($skala['ks8']);
+            $matriks['SR']['EH'] = abs(1 / $skala['ks8']);
+        } else {
+            $matriks['EH']['SR'] = abs(1 / $skala['ks8']);
+            $matriks['SR']['EH'] = abs($skala['ks8']);
+        }
 
-            if ($skala['ks9'] > 0){
-                $matriks['EH']['QK'] = abs($skala['ks9']);
-                $matriks['QK']['EH'] = abs(1/$skala['ks9']);
-            } else {
-                $matriks['EH']['QK'] = abs(1/$skala['ks9']);
-                $matriks['QK']['EH'] = abs($skala['ks9']);
-            }
+        if ($skala['ks9'] > 0) {
+            $matriks['EH']['QK'] = abs($skala['ks9']);
+            $matriks['QK']['EH'] = abs(1 / $skala['ks9']);
+        } else {
+            $matriks['EH']['QK'] = abs(1 / $skala['ks9']);
+            $matriks['QK']['EH'] = abs($skala['ks9']);
+        }
 
-            if ($skala['ks10'] > 0){
-                $matriks['SR']['QK'] = abs($skala['ks10']);
-                $matriks['QK']['SR'] = abs(1/$skala['ks10']);
-            } else {
-                $matriks['SR']['QK'] = abs(1/$skala['ks10']);
-                $matriks['QK']['SR'] = abs($skala['ks10']);
-            }
-//        }
-        var_dump($matriks);
-//        return view('ahp.lihat',$data);
+        if ($skala['ks10'] > 0) {
+            $matriks['SR']['QK'] = abs($skala['ks10']);
+            $matriks['QK']['SR'] = abs(1 / $skala['ks10']);
+        } else {
+            $matriks['SR']['QK'] = abs(1 / $skala['ks10']);
+            $matriks['QK']['SR'] = abs($skala['ks10']);
+        }
+
+        $data = [
+            'kriteria_kuesioner_id' => $id,
+            'kriteria_hr' => json_encode($matriks['HR']),
+            'kriteria_sr' => json_encode($matriks['SR']),
+            'kriteria_cs' => json_encode($matriks['CS']),
+            'kriteria_eh' => json_encode($matriks['EH']),
+            'kriteria_qk' => json_encode($matriks['QK']),
+        ];
+
+        var_dump($data);
+
+        DB::table('matriks_kriteria')->insert($data);
+        alert()->success('Matriks kriteria sudah dibuat', 'Sukses');
+        return redirect('/ahp/'.$id.'/lihat');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -200,8 +210,8 @@ class AhpController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -212,7 +222,7 @@ class AhpController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
