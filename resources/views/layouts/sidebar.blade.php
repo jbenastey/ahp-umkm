f3a1410c18ec1b4a66e679e65a079656202b2646
Jihad Benastey <jbenastey@gmail.com>
2020-04-09 19:19:08 +0700

    fix sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,17 +11,17 @@
             <!-- /menu header -->
 
             <!-- Menu Item -->
-            <li class="dt-side-nav__item">
+            <li class="dt-side-nav__item @if(request()->is('home')) open @endif">
                 <a href="{{url('home')}}" class="dt-side-nav__link" title="Widgets"> <i
                         class="icon icon-dashboard icon-fw icon-lg"></i>
                     <span class="dt-side-nav__text">Dashboard</span> </a>
             </li>
-            <li class="dt-side-nav__item open">
+            <li class="dt-side-nav__item @if(request()->is('master')) open @endif">
                 <a href="javascript:void(0)" class="dt-side-nav__link dt-side-nav__arrow" title="Dashboard">
                     <i class="icon icon-settings icon-fw icon-lg"></i> <span class="dt-side-nav__text">Data Master</span> </a>
 
                 <!-- Sub-menu -->
-                <ul class="dt-side-nav__sub-menu" style="display: block;">
+                <ul class="dt-side-nav__sub-menu" style="@if(request()->is('master')) display:block @endif">
                     <li class="dt-side-nav__item">
                         <a href="{{url('master')}}" class="dt-side-nav__link" title="CRM">
                             <span class="dt-side-nav__text">&nbsp;Data Pernyataan</span> </a>
@@ -30,12 +30,12 @@
                 <!-- /sub-menu -->
 
             </li>
-            <li class="dt-side-nav__item">
+            <li class="dt-side-nav__item @if(request()->is('kuesioner')) open @endif">
                 <a href="{{url('kuesioner')}}" class="dt-side-nav__link" title="Task Manager"> <i
                         class="icon icon-forms-advanced icon-fw icon-lg"></i>
                     <span class="dt-side-nav__text">Data Kuesioner</span> </a>
             </li>
-            <li class="dt-side-nav__item">
+            <li class="dt-side-nav__item @if(request()->is('ahp')) open @endif">
                 <a href="{{url('ahp')}}" class="dt-side-nav__link" title="Task Manager"> <i
                         class="icon icon-chart-line icon-fw icon-lg"></i>
                     <span class="dt-side-nav__text">Analisa AHP</span> </a>
