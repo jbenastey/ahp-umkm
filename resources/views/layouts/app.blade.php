f1c8b3950b6d0ad2b4d341bab6e6f7f85939e982
Jihad Benastey <jbenastey@gmail.com>
2020-10-01 21:11:57 +0700

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -503,12 +503,7 @@ License: You must have a valid license purchased only from themeforest(the above
             </div>
             <!-- END : End Main Content-->
 
-            <!-- BEGIN : Footer-->
-            <footer class="footer undefined undefined">
-                <p class="clearfix text-muted m-0"><span>Copyright  &copy; 2020 &nbsp;</span><a href="../../../themeforest.net/user/pixinvent/portfolioc32a.html?ref=pixinvent" id="pixinventLink" target="_blank">PIXINVENT</a><span class="d-none d-sm-inline-block">, All rights reserved.</span></p>
-            </footer>
-            <!-- End : Footer--><!-- Scroll to top button -->
-            <button class="btn btn-primary scroll-top" type="button"><i class="ft-arrow-up"></i></button>
+            @include('layouts.footer')
 
         </div>
     </div>
