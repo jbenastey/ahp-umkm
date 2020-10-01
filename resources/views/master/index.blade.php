5d7cb197d98476f9d5ebe61397ef3cc98d85ac85
Jihad Benastey <jbenastey@gmail.com>
2020-10-01 21:36:42 +0700

    tampilan kriteria

--- resources/views/master/index.blade.php
+++ resources/views/master/index.blade.php
@@ -10,51 +10,48 @@
             <!-- Grid Item -->
             <div class="col-xl-12">
 
-                <!-- Entry Header -->
-                <div class="dt-entry__header">
-
-                    <!-- Entry Heading -->
-                    <div class="dt-entry__heading">
-                        <h3 class="dt-entry__title">Data Kriteria</h3>
-                    </div>
-                    <!-- /entry heading -->
-
-                </div>
-                <!-- /entry header -->
 
                 <!-- Card -->
-                <div class="dt-card">
+                <div class="card">
+
+                    <!-- Entry Header -->
+                    <div class="card-header">
+                        <h3 class="card-title">Data Kriteria</h3>
+                    </div>
 
                     <!-- Card Body -->
-                    <div class="dt-card__body">
-                        <a href="{{url('master/create')}}" class="btn btn-sm btn-primary mb-5"><i class="icon icon-plus"></i> Tambah Kriteria</a>
-                        <!-- Tables -->
-                        <div class="table-responsive">
-                            <table id="data-table" class="table table-striped table-bordered table-hover zero-configuration">
-                                <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Nama Kriteria</th>
-                                    <th class="text-center"><i class="icon icon-settings"></i></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($kriteria as $value)
+                    <div class="card-content">
+                        <div class="card-body">
+                            <!-- Tables -->
+                            <div class="table-responsive">
+                                <table id="data-table" class="table table-striped table-bordered table-hover zero-configuration">
+                                    <a href="{{url('master/create')}}" class="btn btn-sm btn-primary ml-1 float-right"><i class="icon icon-plus"></i> Tambah Kriteria</a>
+                                    <thead>
                                     <tr>
-                                        <td>{{$loop->iteration}}</td>
-                                        <td>{{$value->kriteria_nama}}</td>
-                                        <td class="text-center">
-                                            <a href="{{action('MasterController@show',$value->kriteria_id)}}" class="btn btn-sm btn-primary">Lihat</a>
-                                        </td>
+                                        <th>No</th>
+                                        <th>Nama Kriteria</th>
+                                        <th class="text-center"><i class="icon icon-settings"></i></th>
                                     </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($kriteria as $value)
+                                        <tr>
+                                            <td>{{$loop->iteration}}</td>
+                                            <td>{{$value->kriteria_nama}}</td>
+                                            <td class="text-center">
+                                                <a href="{{action('MasterController@show',$value->kriteria_id)}}" class="primary" title="Lihat"><i class="ft-eye font-medium-3"></i></a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+
+                            </div>
+                            <!-- /tables -->
 
                         </div>
-                        <!-- /tables -->
-
                     </div>
+
                     <!-- /card body -->
 
                 </div>
